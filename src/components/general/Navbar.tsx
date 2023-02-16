bb5a5092bf794dd3b52dc34058a424cb448425e5
Asahel Enock Kigen <kirwaasahel@gmail.com>
2023-02-17 01:16:22 +0300

    [UPDATE]: navbar update

--- src/components/general/Navbar.tsx
+++ src/components/general/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { AiFillThunderbolt } from "react-icons/ai";
@@ -8,47 +8,73 @@ import { RxCross2 } from "react-icons/rx";
 const Navbar = () => {
   const [openNav, setOpenNav] = useState(false);
 
-  const headerRef = useRef(null);
+  const headerRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-                headerRef?.current?.classList?.add('shrink')
-            } else {
-                headerRef?.current?.classList?.remove('shrink')
-            }
-        })
-    }, [headerRef]);
-
-    const handleClickScroll = (section: string) => {
-      const element = document.getElementById(section);
-      if (element) {
-          // 👇 Will scroll smoothly to the top of the selected section
-          element.scrollIntoView({ behavior: 'smooth' });
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (
+        document?.body?.scrollTop > 80 ||
+        document?.documentElement?.scrollTop > 80
+      ) {
+        headerRef?.current?.classList?.add("shrink");
+      } else {
+        headerRef?.current?.classList?.remove("shrink");
       }
-  }
+    });
+  }, [headerRef]);
+
+  const handleClickScroll = (section: string) => {
+    const element = document.getElementById(section);
+    if (element) {
+      // 👇 Will scroll smoothly to the top of the selected section
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   const navList = (
     <div className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:justify-between lg:gap-6 text-white">
-      <Link className="font-semibold text-lg" to="/#home" onClick={() => handleClickScroll('home')}>
+      <Link
+        className="font-semibold text-lg"
+        to="/#home"
+        onClick={() => handleClickScroll("home")}
+      >
         Home
       </Link>
-      <Link className="font-semibold text-lg" to="/#about" onClick={() => handleClickScroll('about')}>
+      <Link
+        className="font-semibold text-lg"
+        to="/#about"
+        onClick={() => handleClickScroll("about")}
+      >
         About me
       </Link>
-      <Link className="font-semibold text-lg" to="https://drive.google.com/file/d/1HNMKSw1xDAdhK_ighiqQAs6TyaZLIe0a/view?usp=share_link" target="_blank">
+      <Link
+        className="font-semibold text-lg"
+        to="https://drive.google.com/file/d/1HNMKSw1xDAdhK_ighiqQAs6TyaZLIe0a/view?usp=share_link"
+        target="_blank"
+      >
         Resume
       </Link>
-      <Link className="font-semibold text-lg" to="/#projects" onClick={() => handleClickScroll('projects')}>
+      <Link
+        className="font-semibold text-lg"
+        to="/#projects"
+        onClick={() => handleClickScroll("projects")}
+      >
         Projects
       </Link>
-      <Link className="font-semibold text-lg" to="/#contact" onClick={() => handleClickScroll('contact')}>
+      <Link
+        className="font-semibold text-lg"
+        to="/#contact"
+        onClick={() => handleClickScroll("contact")}
+      >
         Contact
       </Link>
     </div>
   );
   return (
-    <Box className="fixed w-full top-0 left-0 z-[99] bg-bg_color text-white" ref={headerRef}>
+    <Box
+      className="fixed w-full top-0 left-0 z-[99] bg-bg_color text-white"
+      ref={headerRef}
+    >
       <Box mx="4" my="4">
         <Box className="mx-auto flex items-center justify-between">
           <Link to="/">
